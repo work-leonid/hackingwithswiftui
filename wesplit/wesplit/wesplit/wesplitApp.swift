1ae1eb57ed8a7a2372477dc7ca5bcbd41743343c
leonid nazarov <work.leonid@gmail.com>
2021-01-29 10:05:18 +0300

    Init new empty project

---
+++ wesplit/wesplit/wesplit/wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  wesplitApp.swift
+//  wesplit
+//
+//  Created by Leonid on 29.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
