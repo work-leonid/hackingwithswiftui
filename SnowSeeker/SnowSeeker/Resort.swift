67e20e8e00991004be5eb1c9210d86f3e7655aef
work-leonid <work.leonid@gmail.com>
2021-04-18 15:58:06 +0300

    add json decode

--- SnowSeeker/SnowSeeker/Resort.swift
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -19,4 +19,9 @@ struct Resort: Codable, Identifiable {
     let elevation: Int
     let runs: Int
     let facilities: [String]
+
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
 }
+
+
