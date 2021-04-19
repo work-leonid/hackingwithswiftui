2d168e2fa4bcdcf780d7b0ce8e5507ff1bbcb989
work-leonid <work.leonid@gmail.com>
2021-04-19 16:07:40 +0300

    init wesplit

---
+++ wesplit/WeSplitMy/WeSplitMy/WeSplitMyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitMyApp.swift
+//  WeSplitMy
+//
+//  Created by Leonid Nazarov on 19.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitMyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
