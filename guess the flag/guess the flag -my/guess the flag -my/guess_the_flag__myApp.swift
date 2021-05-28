f5f195eb6df88138ba42b955f3730d52dd62afb2
work-leonid <work.leonid@gmail.com>
2021-05-28 13:50:35 +0300

    init custom guess the flag

---
+++ guess the flag/guess the flag -my/guess the flag -my/guess_the_flag__myApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flag__myApp.swift
+//  guess the flag -my
+//
+//  Created by Leonid Nazarov on 28.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flag__myApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
