430a863f6cc3cafb4a08ded0f6ad37a59c7c23d7
work-leonid <work.leonid@gmail.com>
2021-05-28 10:22:30 +0300

    init guess the flag project

---
+++ guess the flag/guess the flag original/guess the flag original/guess_the_flag_originalApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flag_originalApp.swift
+//  guess the flag original
+//
+//  Created by Leonid Nazarov on 28.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flag_originalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
